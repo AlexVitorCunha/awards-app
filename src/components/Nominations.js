4145d46dc63472bf698068e9a75796b641409eb7
Alex Cunha <alexvitormc@gmail.com>
2021-05-08 17:28:11 -0300

    final commit

--- src/components/Nominations.js
+++ src/components/Nominations.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React from "react";
 
 const Nominations = (props) => {
-    if(props.movies){
-        return (
-        <>
-        <h3>Nominations</h3>
-        <ul>
-            {props.movies.map((movie, index) => (<li>
-                <p>{movie.Title} ({movie.Year})</p>
-                <button className="delete-nomination" onClick={() => props.handleNominationsClick(movie)}>Delete</button>
-            </li>))}
-        </ul>
-        </>
-    )
-    }
-    return(
-        <>
-        <h3>Nominations</h3>
-        </>  
-    )
-}
+  return (
+    <>
+      <h3>Nominations</h3>
+      <ul>
+        {props.movies.map((movie, index) => (
+          <li>
+            <p>
+              {movie.Title} ({movie.Year})
+            </p>
+            <button
+              className="delete-nomination"
+              onClick={() => props.handleNominationsClick(movie)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default Nominations;
