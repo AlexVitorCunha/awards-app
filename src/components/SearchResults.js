f75785810149c930ab21fae12eb6223d8c55c1d8
Alex Cunha <alexvitormc@gmail.com>
2021-05-08 17:02:22 -0300

    search error

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,15 +1,28 @@
 import React from 'react';
 
 const SearchResults = (props) => {
+    if(props.nominations){
     return (
         <>
         <h3>Results for "{props.currentSearch}"</h3>
         <ul id="result-list"> 
             {props.movies.map((movie, index) => (<li>
                 <p>{movie.Title} ({movie.Year})</p>
-                <button disabled={
+                <button disabled={ 
                     props.nominations.some((nomination) => nomination.imdbID == movie.imdbID)
                 } className="add-nomination" onClick={() => props.handleNominationsClick(movie)}>Nominate</button>
+            </li>))}
+        </ul>
+        </>
+    )
+    }
+    return (
+        <>
+        <h3>Results for "{props.currentSearch}"</h3>
+        <ul id="result-list"> 
+            {props.movies.map((movie, index) => (<li>
+                <p>{movie.Title} ({movie.Year})</p>
+                <button className="add-nomination" onClick={() => props.handleNominationsClick(movie)}>Nominate</button>
             </li>))}
         </ul>
         </>
